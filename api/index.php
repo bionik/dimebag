d4547457d9107e8eecdca9007cdf37e18407560d
Lauri Hakkarainen <bionik@gmail.com>
2016-05-28 22:15:29 +0300

    Implemented some more stuff

--- api/index.php
+++ api/index.php
@@ -1,11 +1,47 @@
 <?php
+//API for :DimeBag
 
-date_default_timezone_set('Europe/Stockholm');
+//Design principles of this API architechture:
+//Always respond in JSON
+//Response data is stored on the $response variable. If response status is not set, show an error
+//On succesful operation, set status as OK, on failure set status as FAIL
 
+//:DimeBag specific principles:
+//Action is chosen with the a (action) parameter of the request
+
+//Disable caching
+header("Pragma: no-cache");
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Thu, 01 Jan 1970 00:00:00 GMT");
+
+//Set response content type
+header("Content-type: application/json");
+
+//Set default timezone
+date_default_timezone_set('Europe/Helsinki');
+
+//Method for an immediate error message, also encoded in JSON
+function crash($message){
+  die(json_encode(array('status'=>'ERROR', 'message'=>$message)));
+
+}
+
+//Checks data is set
+function c($variable){
+  if(isset($variable) && $variable !== '') return true;
+  return false;
+}
+
+//This variable will contain the response sent to the user
+$response = array();
+
+//Try to connect to the database, and create the tables if they do not exist.
 try {
-  // Create (connect to) SQLite database in file
+  //Create or connect to SQLite database in file.
+  //NEEDS WRITE PERMISSIONS TO THE DIRECTORY, if the database file does not already exist.
   $db = new PDO('sqlite:dimebag.sqlite3');
-  // Set errormode to exceptions
+
+  //Set error reporting mode
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   //Create tables
@@ -18,9 +54,30 @@ try {
   );
 
 } catch(PDOException $e) {
-  // Print PDOException message
-  echo $e->getMessage();
+  //On db connection error crash with an error message.
+  crash($e->getMessage());
 
 }
 
-echo "Jea boii";
+//Shorthand for the request
+$r = $_REQUEST;
+
+if(isset($r['a']) && $r['a'] != ''){
+
+  if($r['a'] == 'getUsers'){
+    $response['status'] = "OK";
+    $response['users'] = array(array('id'=>1, 'nick'=>'bionik', 'credit'=>12.00),array('id'=>2, 'nick'=>'netl', 'credit'=>16.00));
+  } else if($r['a'] == 'createUser'){
+    //TODO
+  }
+
+}
+
+if(!isset($response['status'])){
+  //If response status was not set, we can assume a parameter error
+  crash('PARAMETER_ERROR');
+
+}
+
+//Finally, JSON encode and print the response.
+die(json_encode($response));
